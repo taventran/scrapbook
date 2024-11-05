92586931a67deba6e41dfc935b32e87b1eb7adae
taventran <taventran@gmail.com>
2024-11-05 13:11:19 -0600

    First push adding react

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import HomeScreen from "./components/homescreen/homescreen";
+function App() {
+  return (
+    <div className="App">
+      <HomeScreen />
+      <p>Testing</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/homescreen/homescreen.js
@@ -0,0 +1,9 @@
+function HomeScreen() {
+  return (
+    <div className="homescreen">
+      <h1>Testing</h1>
+    </div>
+  );
+}
+
+export default HomeScreen();
