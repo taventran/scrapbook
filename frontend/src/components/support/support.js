fbb9921b14fbd6a92136a5518a79e08aea101af4
taventran <taventran@gmail.com>
2024-12-08 22:42:15 -0600

    Added a buy me a coffee link to the support page

--- frontend/src/components/support/support.js
+++ frontend/src/components/support/support.js
@@ -1,15 +1,26 @@
 import React from "react";
 import "./support.css";
 import Navbar from "../navbar/navbar";
+import { SiBuymeacoffee } from "react-icons/si";
 
 function Support() {
     return ( 
-        <div>
+        <div className="support-container">
             <Navbar/>
             <div className="support">
-                <img src={`${process.env.PUBLIC_URL}/photos/squidward.jpg`} alt="Squidward panhandling"/>
-                <p>Spare change please!</p>
-                <p>Anything helps!</p>
+                <img className="squidward" 
+                  src={
+              `${process.env.PUBLIC_URL}/photos/squidward.jpg`} 
+              alt="Squidward panhandling"
+              />
+                <p>Please donate if you enjoyed this website</p>
+                <a href="https://buymeacoffee.com/taventran">
+                  <div className="coffee"> 
+                    <p> Buy me a coffee </p>
+                    <SiBuymeacoffee style={{fontSize: "2em"}} 
+                    id="coffee-icon" />
+                  </div>
+                </a>
             </div>
         </div>
     )
