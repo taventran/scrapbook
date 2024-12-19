641220eb34c917aa8a959d2daa9f7dcf2a52ec2d
Taven Tran <taventran@tavens-mbp.attlocal.net>
2024-12-18 22:38:05 -0600

    Started some initial designing of homepage

--- frontend/src/components/homescreen/homescreen.js
+++ frontend/src/components/homescreen/homescreen.js
@@ -7,23 +7,40 @@ function HomeScreen() {
   return (
     <div className={`homescreen ${isMobile ? "mobile" : "desktop"}`}>
       <Navbar />
-      <div>
-        <p className="text">
-          Site is still under construction, nothing really works.
-        </p>
-        <p className="text"> Feel free to test out the create tab. </p>
-        {isMobile ? (
-          <div>
-            <p className="text">THIS SITE WILL NOT WORK WELL ON MOBILE</p>
-            <p className="text"> Currently only optimized for computers</p>
-          </div>
-        ) : (
-          <p>(๑>؂•̀๑)</p>
-        )}
-        <img
-          src={`${process.env.PUBLIC_URL}/photos/underconstruction.png`}
-          alt="Under Construction"
+      <div className="page">
+        <h1 id="header">Scrapbook your 
+          <span id="s">
+            s
+          </span>
+          <span id="t">
+            t
+          </span>
+          <span id="o">
+            o
+          </span>
+          <span id="r">
+            r
+          </span>
+          <span id="y">
+            y
+          </span> 
+        </h1>
+        <img id="banner"
+        src={`${process.env.PUBLIC_URL}/photos/blanket.jpg`}
+        alt="picnic-banner"
         />
+        <div className="subheader-container">
+          <h2 id="subheader">
+            Scrapsite makes memories permanent
+          </h2>
+        </div>
+        
+        <div className="tutorial">
+          <h2>How to use</h2>
+          <iframe width="560" height="315" 
+            src="https://www.youtube.com/embed/uAkd63_IiLY?si=7wX1Gd_Zxp86BJUM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+          </iframe>
+        </div>
       </div>
     </div>
   );
