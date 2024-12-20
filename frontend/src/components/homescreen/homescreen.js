d359e768d48fa4a34efadefa270edc3f49a18db8
Taven Tran <taventran@Tavens-MacBook-Pro.local>
2024-12-20 15:22:07 -0600

    Changed colors of toolbar and background color of editor

--- frontend/src/components/homescreen/homescreen.js
+++ frontend/src/components/homescreen/homescreen.js
@@ -38,7 +38,13 @@ function HomeScreen() {
         <div className="tutorial">
           <h2>How to use</h2>
           <iframe width="560" height="315" 
-            src="https://www.youtube.com/embed/uAkd63_IiLY?si=7wX1Gd_Zxp86BJUM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+            src="https://www.youtube.com/embed/uAkd63_IiLY?si=7wX1Gd_Zxp86BJUM" 
+            title="YouTube video player"
+            frameborder="0" allow="accelerometer; autoplay; 
+            clipboard-write; encrypted-media; gyroscope; 
+            picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen>
           </iframe>
         </div>
       </div>
