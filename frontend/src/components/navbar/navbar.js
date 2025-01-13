1e57042b0d3b230cb73aa4ef4871d744d119cac5
Taven Tran <taventran@Tavens-MacBook-Pro.local>
2025-01-12 20:12:52 -0600

    Adjusted homescreen look and added hamburger menu to navbar

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,23 +1,36 @@
+import React, { useState } from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="nav">
-      <Link className="logo" to="/">
-        Scrapsite
-      </Link>
-      <Link className="item" to="/">
-        Home
-      </Link>
-      <Link className="item" to="/create">
-        Create
-      </Link>
-      <Link className="item" to="/about">
-        About
-      </Link>
+      <div className="logo">
+        <Link to="/">Scrapsite</Link>
+      </div>
+      <div className={`menu-toggle ${isMenuOpen ? "open" : ""}`} onClick={toggleMenu}>
+        ☰
+      </div>
+      <div className={`menu-items ${isMenuOpen ? "show" : ""}`}>
+        <Link className="item" to="/" onClick={toggleMenu}>
+          Home
+        </Link>
+        <Link className="item" to="/create" onClick={toggleMenu}>
+          Create
+        </Link>
+        <Link className="item" to="/about" onClick={toggleMenu}>
+          About
+        </Link>
+      </div>
     </nav>
   );
 }
 
 export default Navbar;
+
